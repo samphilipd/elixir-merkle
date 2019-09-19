5d3df346571a2e7bfdfd6480463fccc6dcd45514
Sam Davies <seivadmas@gmail.com>
2019-09-19 07:45:12 +0100

    initial

---
+++ lib/merkle.ex
@@ -0,0 +1,18 @@
+defmodule Merkle do
+  @moduledoc """
+  Documentation for Merkle.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Merkle.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/merkle_test.exs
@@ -0,0 +1,8 @@
+defmodule MerkleTest do
+  use ExUnit.Case
+  doctest Merkle
+
+  test "greets the world" do
+    assert Merkle.hello() == :world
+  end
+end
